175f13f737d4e2b862ee8813a279dfceef972516
tasawur92 <tasawur92@gmail.com>
2021-06-20 18:23:20 +0500

    first list finished

---
+++ src/books.js
@@ -0,0 +1,69 @@
+export const books = [
+  {
+    id: 1,
+    img: 'https://m.media-amazon.com/images/I/81eB+7+CkUL._AC_UY218_.jpg',
+    title: 'I Love You To The Moon and Back',
+    author: 'Amelia Hepworth',
+  },
+  {
+    id: 2,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/713mzPe9SwS._AC_UL200_SR200,200_.jpg',
+    title:
+      'Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones',
+    author: 'James Clear',
+  },
+  {
+    id: 3,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81l1UM7a4bS._AC_UL200_SR200,200_.jpg',
+    title: 'American Marxism',
+    author: 'Mark R. Levin ',
+  },
+  {
+    id: 4,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/610O4MLOT2L._AC_UL200_SR200,200_.jpg',
+    title: 'Greenlights',
+    author: 'Mathew McConaughey',
+  },
+  {
+    id: 5,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/91g%2BaK0nCnL._AC_UL200_SR200,200_.jpg',
+    title: 'The Four Winds',
+    author: 'kristin Hannah',
+  },
+  {
+    id: 6,
+    img: 'https://m.media-amazon.com/images/I/81MVfud0o8L._AC_UY218_.jpg',
+    title: 'Caste (Oprahs Book Club',
+    author: 'Isabel Wilkerson',
+  },
+  {
+    id: 7,
+    img: 'https://m.media-amazon.com/images/I/71xS4UfKNiL._AC_UY218_.jpg',
+    title: 'The Plant-Based Athlete ',
+    author: 'Matt Frazier',
+  },
+  {
+    id: 8,
+    img: 'https://m.media-amazon.com/images/I/511z0lqYw9L._AC_UY218_.jpg',
+    title: 'Cynical Theories',
+    author: 'Helen Pluckrose',
+  },
+  {
+    id: 9,
+    img: 'https://m.media-amazon.com/images/I/81LKc4LjZOL._AC_UY218_.jpg',
+    title: 'Confronting Injustice without Compromising Truth',
+    author: 'Thaddeus J. Willams',
+  },
+  {
+    id: 10,
+    img: 'https://m.media-amazon.com/images/I/91FnyIYiqRL._AC_UY218_.jpg',
+    title: 'Live Not by Lie',
+    author: 'Rod Dreher',
+  },
+  {
+    id: 11,
+    img: 'https://m.media-amazon.com/images/I/710Cf7Km56L._AC_UY218_.jpg',
+    title: 'Fault Lines',
+    author: 'Voddie T.Baucham Jr',
+  },
+]
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDom from 'react-dom'
 
 //css
 import './index.css'
+import {books} from './books'
+import {greeting} from './tutorial/testing'
 // sateless function component
 // always return jsx
 // first function letter is always captitalized
@@ -16,24 +18,75 @@ import './index.css'
 // formatting
 
 // setup var
-const books = [
-  {
-    img: 'https://m.media-amazon.com/images/I/81eB+7+CkUL._AC_UY218_.jpg',
-    title: 'I Love You To The Moon and Back',
-    author: 'Amelia Hepworth',
-  },
-  {
-    img: 'https://images-na.ssl-images-amazon.com/images/I/713mzPe9SwS._AC_UL200_SR200,200_.jpg',
-    title:
-      'Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones',
-    author: 'James Clear',
-  },
-  {
-    img: 'https://images-na.ssl-images-amazon.com/images/I/81l1UM7a4bS._AC_UL200_SR200,200_.jpg',
-    title: 'American Marxism',
-    author: 'Mark R. Levin ',
-  },
-]
+// const books = [
+//   {
+//     id: 1,
+//     img: 'https://m.media-amazon.com/images/I/81eB+7+CkUL._AC_UY218_.jpg',
+//     title: 'I Love You To The Moon and Back',
+//     author: 'Amelia Hepworth',
+//   },
+//   {
+//     id: 2,
+//     img: 'https://images-na.ssl-images-amazon.com/images/I/713mzPe9SwS._AC_UL200_SR200,200_.jpg',
+//     title:
+//       'Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones',
+//     author: 'James Clear',
+//   },
+//   {
+//     id: 3,
+//     img: 'https://images-na.ssl-images-amazon.com/images/I/81l1UM7a4bS._AC_UL200_SR200,200_.jpg',
+//     title: 'American Marxism',
+//     author: 'Mark R. Levin ',
+//   },
+//   {
+//     id: 4,
+//     img: 'https://images-na.ssl-images-amazon.com/images/I/610O4MLOT2L._AC_UL200_SR200,200_.jpg',
+//     title: 'Greenlights',
+//     author: 'Mathew McConaughey',
+//   },
+//   {
+//     id: 5,
+//     img: 'https://images-na.ssl-images-amazon.com/images/I/91g%2BaK0nCnL._AC_UL200_SR200,200_.jpg',
+//     title: 'The Four Winds',
+//     author: 'kristin Hannah',
+//   },
+//   {
+//     id: 6,
+//     img: 'https://m.media-amazon.com/images/I/81MVfud0o8L._AC_UY218_.jpg',
+//     title: 'Caste (Oprahs Book Club',
+//     author: 'Isabel Wilkerson',
+//   },
+//   {
+//     id: 7,
+//     img: 'https://m.media-amazon.com/images/I/71xS4UfKNiL._AC_UY218_.jpg',
+//     title: 'The Plant-Based Athlete ',
+//     author: 'Matt Frazier',
+//   },
+//   {
+//     id: 8,
+//     img: 'https://m.media-amazon.com/images/I/511z0lqYw9L._AC_UY218_.jpg',
+//     title: 'Cynical Theories',
+//     author: 'Helen Pluckrose',
+//   },
+//   {
+//     id: 9,
+//     img: 'https://m.media-amazon.com/images/I/81LKc4LjZOL._AC_UY218_.jpg',
+//     title: 'Confronting Injustice without Compromising Truth',
+//     author: 'Thaddeus J. Willams',
+//   },
+//   {
+//     id: 10,
+//     img: 'https://m.media-amazon.com/images/I/91FnyIYiqRL._AC_UY218_.jpg',
+//     title: 'Live Not by Lie',
+//     author: 'Rod Dreher',
+//   },
+//   {
+//     id: 11,
+//     img: 'https://m.media-amazon.com/images/I/710Cf7Km56L._AC_UY218_.jpg',
+//     title: 'Fault Lines',
+//     author: 'Voddie T.Baucham Jr',
+//   },
+// ]
 // const names = ['john', 'peter', 'susan']
 // const newNames = names.map((name) => {
 //   return <h1>{name}</h1>
@@ -43,14 +96,14 @@ const books = [
 // const author = 'Amelia Hepworth'
 // const title = 'I Love You To The Moon and Back'
 // const img = 'https://m.media-amazon.com/images/I/81eB+7+CkUL._AC_UY218_.jpg'
-
+console.log(greeting);
 function Booklist() {
   return (
     <section className='booklist'>
-      {books.map((book) => {
+      {books.map((book, index) => {
         // console.log(book)
-        const { img, title, author } = book
-        return <Book book={book}></Book>
+        // const { img, title, author } = book
+        return <Book key={book.id} {...book}></Book>
       })}
       {/* {newNames} */}
       {/* <Book
@@ -101,15 +154,40 @@ function Booklist() {
     // </div>
   )
 }
-const Book = (props) => {
-  const { img, title, author } = props.book
 
+const Book = ({ img, title, author }) => {
+  // const { img, title, author } = props.book
   // console.log(props)
+
+  // attribute, eventHandler
+  // onClick, onMouseHover
+  const clickHandler = (e) => {
+    console.log(e);
+    console.log(e.target);
+    alert('hello world')
+  }
+
+  const complexExample =(author) => {
+    console.log(author);
+  }
+
+  const moreImage =(img) => {
+    console.log(img);
+  }
+
   return (
-    <article className='book'>
+    <article className='book' onMouseOver={()=>{
+      console.log(title);
+    }
+    }>
       <img src={img} alt='' />
-      <h1>{title}</h1>
+      <h1 onClick={() => console.log(title)}>{title}</h1>
       <h4>{author}</h4>
+      <button type='button' onClick={clickHandler}>
+        reference example
+      </button>
+      <button type='button' onClick={()=>complexExample(author)}>more complex example</button>
+      <button type='button' onClick={()=>moreImage(img)}>more image</button>
       {/* {children} */}
       {/* /* <Image></Image>
       <Title />
@@ -151,5 +229,6 @@ const Book = (props) => {
 //     React.createElement('h1', {}, 'hello world')
 //   )
 // }
+
 
 ReactDom.render(<Booklist />, document.getElementById('root'))
